754f1b7d32dd5e504a7052f3d09665a8e4a5dc57
Rafał Chłodnicki <rchl2k@gmail.com>
2020-03-29 10:19:53 +0200

    Reformatting and linting

--- SublimeClangFormat.py
+++ SublimeClangFormat.py
@@ -19,28 +19,31 @@ import subprocess
 
 PREF_CLANG_FORMAT_PATH = 'clang_format_path'
 PREF_FILE_NAME = 'SublimeClangFormat (%s).sublime-settings'
+MISSING_BINARY_MESSAGE = 'SublimeClangFormat\n\nTo format the code, either full path to the \
+clang-format binary must be specified in the package settings or %s binary must be in the PATH!'
 
 
 def platform_name():
-  if 'linux' in sys.platform:
-    return 'linux'
-  elif 'darwin' in sys.platform:
-    return 'mac'
-  return 'windows'
+    if 'linux' in sys.platform:
+        return 'linux'
+    elif 'darwin' in sys.platform:
+        return 'mac'
+    return 'windows'
 
 
 def settings_filename():
-  if 'linux' in sys.platform:
-    return PREF_FILE_NAME % 'Linux'
-  elif 'darwin' in sys.platform:
-    return PREF_FILE_NAME % 'OSX'
-  return PREF_FILE_NAME % 'Windows'
+    if 'linux' in sys.platform:
+        return PREF_FILE_NAME % 'Linux'
+    elif 'darwin' in sys.platform:
+        return PREF_FILE_NAME % 'OSX'
+    return PREF_FILE_NAME % 'Windows'
 
 
 def binary_name():
-  if 'win32' in sys.platform:
-    return 'clang-format.exe'
-  return 'clang-format'
+    if 'win32' in sys.platform:
+        return 'clang-format.exe'
+    return 'clang-format'
+
 
 # Change this to format according to other formatting styles
 # (see clang-format -help).
@@ -48,74 +51,66 @@ style = 'Chromium'
 
 
 def is_exe(fpath):
-  return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
 
 def which(program):
-  fpath, fname = os.path.split(program)
-  if fpath:
-    if is_exe(program):
-      return program
-  else:
-    for path in os.environ['PATH'].split(os.pathsep):
-      path = path.strip('"')
-      exe_file = os.path.join(path, program)
-      if is_exe(exe_file):
-        return exe_file
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ['PATH'].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
 
-  return None
+    return None
 
 
 class ClangFormatCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    settings = sublime.load_settings(settings_filename())
-    path = settings.get(PREF_CLANG_FORMAT_PATH)
-    if path:
-      binary_path = path
-    else:
-      binary_path = which(binary_name())
-    if not binary_path or not is_exe(binary_path):
-      sublime.message_dialog(
-          'SublimeClangFormat\n\nTo format the code, either full path to the ' +
-          'clang-format binary must be specified in the package settings or ' +
-          binary_name() + ' binary must be in the PATH!')
-      return
-    encoding = self.view.encoding()
-    if encoding == 'Undefined':
-      encoding = 'utf-8'
-    regions = []
-    args = [binary_path, '-fallback-style', style]
-    if self.view.file_name():
-      args.extend(['-assume-filename', self.view.file_name()])
-    else:
-      print('Checking style without knowing file type. Results might be '
-            'innacurate!')
-    for region in self.view.sel():
-      regions.append(region)
-      region_offset = min(region.a, region.b)
-      region_length = abs(region.b - region.a)
-      args.extend(['-offset', str(region_offset),
-                   '-length', str(region_length)])
-    old_viewport_position = self.view.viewport_position()
-    buf = self.view.substr(sublime.Region(0, self.view.size()))
-    startupinfo = None
-    # Don't let console window pop-up on Windows.
-    if platform_name() == 'windows':
-      startupinfo = subprocess.STARTUPINFO()
-      startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-      startupinfo.wShowWindow = subprocess.SW_HIDE
-    p = subprocess.Popen(args,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         stdin=subprocess.PIPE,
-                         startupinfo=startupinfo)
-    output, error = p.communicate(buf.encode(encoding))
-    if not error:
-      self.view.replace(edit,
-                        sublime.Region(0, self.view.size()),
-                        output.decode(encoding))
-      # FIXME: Without the 10ms delay, the viewport sometimes jumps.
-      sublime.set_timeout(lambda: self.view.set_viewport_position(
-          old_viewport_position, False), 10)
-    else:
-      print(error)
+    def run(self, edit):
+        settings = sublime.load_settings(settings_filename())
+        path = settings.get(PREF_CLANG_FORMAT_PATH)
+        if path:
+            binary_path = path
+        else:
+            binary_path = which(binary_name())
+        if not binary_path or not is_exe(binary_path):
+            sublime.message_dialog(MISSING_BINARY_MESSAGE % binary_name())
+            return
+        encoding = self.view.encoding()
+        if encoding == 'Undefined':
+            encoding = 'utf-8'
+        regions = []
+        args = [binary_path, '-fallback-style', style]
+        if self.view.file_name():
+            args.extend(['-assume-filename', self.view.file_name()])
+        else:
+            print('Checking style without knowing file type. Results might be innacurate!')
+        for region in self.view.sel():
+            regions.append(region)
+            region_offset = min(region.a, region.b)
+            region_length = abs(region.b - region.a)
+            args.extend(['-offset', str(region_offset), '-length', str(region_length)])
+        old_viewport_position = self.view.viewport_position()
+        buf = self.view.substr(sublime.Region(0, self.view.size()))
+        startupinfo = None
+        # Don't let console window pop-up on Windows.
+        if platform_name() == 'windows':
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = subprocess.SW_HIDE
+        p = subprocess.Popen(args,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             stdin=subprocess.PIPE,
+                             startupinfo=startupinfo)
+        output, error = p.communicate(buf.encode(encoding))
+        if not error:
+            self.view.replace(edit, sublime.Region(0, self.view.size()), output.decode(encoding))
+            # FIXME: Without the 10ms delay, the viewport sometimes jumps.
+            sublime.set_timeout(lambda: self.view.set_viewport_position(old_viewport_position, False), 10)
+        else:
+            print(error)
